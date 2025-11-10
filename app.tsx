f65702ef65cf13e74427a1ce77792ee7ccca08e5
CatsAreFluffy <catisfluffy@gmail.com>
2025-11-09 19:21:26 -0800

    Basic parser

--- app.tsx
+++ app.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactCodeMirror from "@uiw/react-codemirror";
 
-import { lex } from "./parser";
+import { lex, parse } from "./parser";
+
+const default_code = `
+ lda #$03
+ sta 20`.replace("\n","");
 
 function App() {
     const onChange = React.useCallback((val, viewUpdate) => {
         console.log("val:", val);
-        console.log("lex:", lex(val));
+        let tokens = lex(val)
+        console.log("lex:", tokens);
+        try {
+            console.log("parse:", parse(tokens));
+        } catch(e) {
+            console.error(e);
+        }
     }, [])
-    return <ReactCodeMirror onChange={onChange}/>;
+    return <ReactCodeMirror value={default_code} onChange={onChange}/>;
 }
 export default App;
--- parser.ts
+++ parser.ts
@@ -1,5 +1,6 @@
 type Token = {
     token: string,
+    kind: "identifier" | "symbol" | "indent" | "newline" | "eof",
     line: number,
     column: number,
 }
@@ -8,31 +9,29 @@ function lex(input: string): Token[] {
     let line = 1;
     let column = 1;
     let ret: Token[] = [];
-    while(input.length) {
-        let match = /^([ \t]+|(?:[ \t]*\n)+|[0-9a-zA-Z_.$]+|[\(\)\[\]+-\/*|^&,#:])(.*)$/s.exec(input);
+    let regexp = /([ \t]+|(?:[ \t]*\n)+(?:[ \t]+$)?|[0-9a-zA-Z_.$]+|<<|>>|[\(\)\[\]+-\/*|^&,#:])/sy;
+    regexp.lastIndex = 0;
+    while(regexp.lastIndex < input.length) {
+        let match = regexp.exec(input);
         if(!match) {
             console.error("No match for", input);
             return ret;
         }
 
         let token = match[1];
-        let result = {token, line, column}
         // simplify whitespace tokens
         if(/\s/.exec(token)) {
-            // if(/\n[ \t].*$/.exec(input)) {
-            //     result.token = "\n ";
-            //     ret.push(result);
-            // } else 
             if(token.includes("\n")) {
-                result.token = "\n";
-                ret.push(result);
+                ret.push({token, line, column, kind: "newline"});
             }
             else if(column == 1) {
                 // single-line whitespace is only significant at the beginning
-                ret.push(result);
+                ret.push({token, line, column, kind: "indent"});
             }
+        } else if(/[0-9a-zA-Z_.$]/.exec(token)) {
+            ret.push({token, line, column, kind: "identifier"});
         } else {
-            ret.push(result);
+            ret.push({token, line, column, kind: "symbol"});
         }
         if(token.includes("\n")) {
             line += token.replaceAll(/[^\n]/g, "").length;
@@ -40,15 +39,107 @@ function lex(input: string): Token[] {
         } else {
             column += token.length;
         }
+    }
+    ret.push({token: "", line, column, kind: "eof"});
+    return ret;
+}
 
-        let rest = match[2];
-        if(rest.length >= input.length) {
-            console.error("Length increased after token", input, rest);
-            return ret;
+class TokenStream {
+    tokens: Token[];
+    index: number;
+
+    constructor(tokens: Token[]) {
+        this.tokens = tokens;
+        this.index = 0;
+    }
+
+    peek(): Token {
+        if(!this.eof()) {
+            return this.tokens[this.index];
         }
-        input = rest;
+        throw new ParseError("Unexpected end of file", this.tokens.at(-1)!);
+    }
+
+    next(): Token {
+        let token = this.peek();
+        this.index++;
+        return token;
+    }
+
+    eof(): boolean {
+        return this.index == this.tokens.length - 1;
+    }
+}
+
+class ParseError extends Error {
+    line: number;
+    column: number;
+
+    constructor(message: string, token: Token) {
+        super(message);
+        this.line = token.line;
+        this.column = token.column;
+    }
+}
+
+type Operand = {mode: "immediate" | "absolute", body: number}
+function parse_operand(stream: TokenStream): Operand {
+    let mode: "immediate" | "absolute" = "absolute";
+    if(stream.peek().token == "#") {
+        mode = "immediate";
+        stream.next();
+    }
+    let value = stream.next();
+    if(value.kind == "newline") {
+        throw new ParseError("Instructions must have arguments", value);
+    }
+    if(value.kind == "symbol") {
+        throw new ParseError("Instruction arguments must not be symbols", value);
+    }
+    let body = 0;
+    if(value.token[0] == "$") {
+        body = parseInt(value.token.slice(1), 16);
+    } else {
+        body = parseInt(value.token);
+    }
+    if(isNaN(body)) {
+        throw new ParseError("Invalid number", value);
+    }
+    return {mode, body};
+}
+
+type Line = [string, Operand]
+function parse_line(stream: TokenStream): Line {
+    let indent = stream.next();
+    if(indent.kind != "indent") {
+        throw new ParseError("Instructions must be indented", indent);
+    }
+    let opcode = stream.next();
+    if(opcode.kind != "identifier") {
+        throw new ParseError("Instructions must not be symbols", opcode);
+    }
+    return [opcode.token, parse_operand(stream)];
+}
+
+type Program = Line[]
+function parse_program(stream: TokenStream): Program {
+    let ret = [];
+    while(!stream.eof()) {
+        if(stream.peek().kind == "newline") {
+            stream.next();
+        }
+        if(stream.eof()) {
+            break;
+        }
+        let line = parse_line(stream);
+        console.log("prog", line, stream.index);
+        ret.push(line);
     }
     return ret;
 }
 
-export { lex };
+function parse(tokens: Token[]): Program {
+    return parse_program(new TokenStream(tokens));
+}
+
+export { lex, parse, Operand, Line, Program };
