524c71fdbac0c40750b1cd1f87f27d044408a2d2
CatsAreFluffy <catisfluffy@gmail.com>
2025-11-11 20:56:05 -0800

    Neater memory viewer

--- memory_view.tsx
+++ memory_view.tsx
@@ -1,20 +1,20 @@
 import React, { ReactElement } from "react";
 
 function MemoryLine({ memory, index, length }: {memory: number[], index: number, length: number}): ReactElement {
-    let ret = [<td key={-1}>{index.toString(16).padStart(4, "0")}:</td>];
+    let ret = [];
     for(let i = 0; i < length; i++) {
         ret.push(<td key={i}>{memory[index + i].toString(16).padStart(2, "0")}</td>);
     }
-    return <tr key={index}>{ret}</tr>;
+    return <tr key={index}><td>{index.toString(16).padStart(4, "0")}:</td>{ret}</tr>;
 }
 
 export default function MemoryView({ memory }: {memory: number[]}) {
-    let top_line = [<td key={-1}></td>];
+    let top_line = [];
     let length = 32;
     for(let i = 0; i < length; i++) {
         top_line.push(<td key={i}>{i.toString(16).padStart(2, "0")}</td>);
     }
-    let ret = [<tr key={-1}>{top_line}</tr>];
+    let ret = [];
     for(let i = 0; i + length - 1 < memory.length; i += length) {
         let nonzero = false;
         for(let j = 0; j < length; j++) {
@@ -28,5 +28,5 @@ export default function MemoryView({ memory }: {memory: number[]}) {
         }
         ret.push(<MemoryLine memory={memory} index={i} length={length} />);
     }
-    return <table><tbody>{ret}</tbody></table>;
+    return <table><tbody><tr><td></td>{top_line}</tr>{ret}</tbody></table>;
 }
