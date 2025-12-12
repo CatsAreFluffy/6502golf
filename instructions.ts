4d213dd9eb889b71f1b417a33a0674aaf4620b56
CatsAreFluffy <catisfluffy@gmail.com>
2025-12-12 04:26:31 -0800

    Fix zeropage LSR

--- instructions.ts
+++ instructions.ts
@@ -60,7 +60,7 @@ const assemble_modes: Map<AddressingMode, AssembleAddressingMode> = new Map([
     ["indirect,y fast", "indirect,y"],
 ]);
 
-type CommonInstruction = "brk" | "ora" | "jam" | "slo" | "nop" | "asl" | "php" | "anc" | "bpl" | "clc" | "jsr" | "and" | "rla" | "bit" | "rol" | "plp" | "bmi" | "sec" | "rti" | "eor" | "sre" | "lse" | "pha" | "alr" | "jmp" | "lsr" | "bvc" | "cli" | "rts" | "adc" | "rra" | "ror" | "pla" | "arr" | "bvs" | "sei" | "sta" | "sax" | "sty" | "stx" | "dey" | "txa" | "ane" | "bcc" | "sha" | "tya" | "txs" | "tas" | "shy" | "shx" | "ldy" | "lda" | "ldx" | "lax" | "tay" | "tax" | "lxa" | "bcs" | "clv" | "tsx" | "las" | "cpy" | "cmp" | "dcp" | "dec" | "iny" | "dex" | "sbx" | "bne" | "cld" | "cpx" | "sbc" | "isc" | "inc" | "inx" | "beq" | "sed";
+type CommonInstruction = "brk" | "ora" | "jam" | "slo" | "nop" | "asl" | "php" | "anc" | "bpl" | "clc" | "jsr" | "and" | "rla" | "bit" | "rol" | "plp" | "bmi" | "sec" | "rti" | "eor" | "sre" | "pha" | "alr" | "jmp" | "lsr" | "bvc" | "cli" | "rts" | "adc" | "rra" | "ror" | "pla" | "arr" | "bvs" | "sei" | "sta" | "sax" | "sty" | "stx" | "dey" | "txa" | "ane" | "bcc" | "sha" | "tya" | "txs" | "tas" | "shy" | "shx" | "ldy" | "lda" | "ldx" | "lax" | "tay" | "tax" | "lxa" | "bcs" | "clv" | "tsx" | "las" | "cpy" | "cmp" | "dcp" | "dec" | "iny" | "dex" | "sbx" | "bne" | "cld" | "cpx" | "sbc" | "isc" | "inc" | "inx" | "beq" | "sed";
 // USBC is executed as SBC
 export type AssembleInstruction = CommonInstruction | "usbc";
 // Accumumator RMW instructions are executed differently from memory RMW instructions
@@ -71,8 +71,8 @@ export const instructions: Instruction[] = [
     "jsr", "and", "jam", "rla",  "bit", "and", "rol", "rla",  "plp", "and", "rola","anc",  "bit", "and", "rol", "rla",
     "bmi", "and", "jam", "rla",  "nop", "and", "rol", "rla",  "sec", "and", "nopa","rla",  "nop", "and", "rol", "rla",
 
-    "rti", "eor", "jam", "sre",  "nop", "eor", "lse", "sre",  "pha", "eor", "lsra","alr",  "jmp", "eor", "lsr", "sre",
-    "bvc", "eor", "jam", "sre",  "nop", "eor", "lse", "sre",  "cli", "eor", "nopa","sre",  "nop", "eor", "lsr", "sre",
+    "rti", "eor", "jam", "sre",  "nop", "eor", "lsr", "sre",  "pha", "eor", "lsra","alr",  "jmp", "eor", "lsr", "sre",
+    "bvc", "eor", "jam", "sre",  "nop", "eor", "lsr", "sre",  "cli", "eor", "nopa","sre",  "nop", "eor", "lsr", "sre",
     "rts", "adc", "jam", "rra",  "nop", "adc", "ror", "rra",  "pla", "adc", "rora","arr",  "jmp", "adc", "ror", "rra",
     "bvs", "adc", "jam", "rra",  "nop", "adc", "ror", "rra",  "sei", "adc", "nopa","rra",  "nop", "adc", "ror", "rra",
 
