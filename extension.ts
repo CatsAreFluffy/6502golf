e93c44f387ad5cb4e03f0ce88f772986f3d101c1
CatsAreFluffy <catisfluffy@gmail.com>
2025-12-14 01:16:45 -0800

    Show error message in tooltip

--- extension.ts
+++ extension.ts
@@ -1,13 +1,13 @@
 import { EditorState, RangeSet, StateEffect, StateField } from "@codemirror/state";
-import { Decoration, DecorationSet, EditorView, ViewPlugin, keymap } from "@codemirror/view";
+import { Decoration, DecorationSet, EditorView, ViewPlugin, hoverTooltip, keymap } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 
 const error_decoration = Decoration.mark({class: "error"});
 
-export const set_error_field = StateEffect.define<[boolean, number, number]>();
+export const set_error_field = StateEffect.define<[boolean, number, number, string]>();
 
-export const error_field = StateField.define<[boolean, number, number]>({
-    create() {return [false, 0, 0];},
+export const error_field = StateField.define<[boolean, number, number, string]>({
+    create() {return [false, 0, 0, ""];},
     update(value, transaction) {
         for(let e of transaction.effects) {
             if(e.is(set_error_field)) {
@@ -33,3 +33,20 @@ export const error_extension = ViewPlugin.define((view) => {
 }, {
     decorations: v => v.decorations,
 });
+
+export const error_tooltip = hoverTooltip((view, pos, side) => {
+    const field = view.state.field(error_field);
+    if(!field[0] || field[1] == field[2] || pos < field[1] || field[2] <= pos) {
+        return null;
+    }
+    return {
+        pos: field[1],
+        end: field[2],
+        above: true,
+        create(view) {
+            let dom = document.createElement("div")
+            dom.textContent = field[3];
+            return {dom}
+        }
+    };
+})
